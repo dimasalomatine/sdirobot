6a54d48ac857aaddbe36fd993159e8340d2c5ff1
dimasalomatine <dima@impactsoft.co.il>
2017-01-31 12:14:18 +0200

    first

--- MUService/src/java/webapi/InitStatic.java
+++ MUService/src/java/webapi/InitStatic.java
@@ -1,12 +1,32 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2017 dmitry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package webapi;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingEnumeration;
@@ -29,6 +49,48 @@ public class InitStatic extends HttpServlet {
     private static final CManagerMap _m_conn = new CManagerMap();
     private static Context _ctx;
     private static ServletContext _ctx2;
+
+    static List<String[]> getUsers(String _ut) {
+        List<String[]> ret=new ArrayList<>(3);
+        ResultSet rs=null;
+        Statement stmt=null;
+        try {
+            Connection conn = CManagerMap.getCon(Info.dbcon);
+             stmt = conn.createStatement();
+             String q="";
+             if("su".equalsIgnoreCase(_ut)){
+             q="SELECT id,atype,name,pv FROM tbl_init";
+             }else{
+               q="SELECT id,atype,name,pv FROM tbl_init WHERE atype = \""+_ut+"\"";
+             }
+             rs = stmt.executeQuery(q);
+            if(rs!=null){ 
+                while(rs.next())
+                {
+                String id=rs.getString(1);
+                String atype=rs.getString(2);
+                String name=rs.getString(3);
+                String pv=rs.getString(4);
+                ret.add(new String[]{id,atype,name,pv});
+            }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InitStatic.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+           if(stmt!=null)try {
+               stmt.close();
+           } catch (SQLException ex) {
+               Logger.getLogger(InitStatic.class.getName()).log(Level.SEVERE, null, ex);
+           }
+           if(rs!=null)try {
+               rs.close();
+           } catch (SQLException ex) {
+               Logger.getLogger(InitStatic.class.getName()).log(Level.SEVERE, null, ex);
+           }
+        }
+        return ret;
+    }
     private  ServletConfig sc = getServletConfig();
 
     public InitStatic(){
@@ -40,27 +102,37 @@ public class InitStatic extends HttpServlet {
     
     String __initpi4j = getServletConfig().getInitParameter("initpi4j");
     String __testenv = getServletConfig().getInitParameter("env");
-    String __dbtype = getServletConfig().getInitParameter("dbtype");
+    Info.dbtype = getServletConfig().getInitParameter("dbtype");
     
-    Info.dbcon=(!__testenv.isEmpty()?__testenv+"_":"")+"rpirobotapp"+"_"+__dbtype;
+    Info.dbcon=(!__testenv.isEmpty()?__testenv+"_":"")+"rpirobotapp"+"_"+Info.dbtype;
     
     if(Boolean.parseBoolean(__initpi4j)){
      StaticHolder.getInstance();
     }
 
         try {
+            if(sc==null){
+            sc = getServletConfig();
+            }
             if(sc!=null)
             {
             _ctx2 = sc.getServletContext();
             _ctx = new InitialContext();
             _ctx = (Context) _ctx.lookup("java:comp/env");
             NamingEnumeration e = _ctx.listBindings("jdbc");
-            CManagerMap.enumerate(_ctx, e, "jdbc");
+            CManagerMap.enumerate(_ctx, e, "jdbc",Info.dbtype);
             _ctx.close();
             }
         } catch (NamingException n) {
             System.out.println(n.getMessage());
         }
+        
+        Connection conn = CManagerMap.getCon(Info.dbcon);
+        if("sqlite".equals(Info.dbtype)){
+         loaddb(conn);
+        }
+        
+        
     
     }
     @Override
@@ -71,18 +143,22 @@ public class InitStatic extends HttpServlet {
     
     
     }
-    @Override
+   /* @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         if(sc==null)sc=config;
         initmy();
-    }
+    }*/
     
-    @Override
+    /*@Override
     public void destroy(){
         //do
+    Connection conn = CManagerMap.getCon(Info.dbcon);
+        if("sqlite".equals(Info.dbtype)){
+         savedb(conn);
+        }
     super.destroy();
-    }
+    }*/
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -150,5 +226,92 @@ public class InitStatic extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+    private boolean loaddb(Connection conn) {
+        boolean ret=false;
+        ResultSet rs=null;
+        Statement stmt=null;
+        String path=System.getProperty("user.dir")+File.separator+"rpirobotapp.db";
+        
+        try {
+            stmt = conn.createStatement();
+            File fp=new File(path);
+            if(fp.exists()){
+            stmt.execute("restore from "+path);
+            }else{
+              stmt.execute("CREATE TABLE tbl_init(id INTEGER, atype VARCHAR(20), name VARCHAR(20), pv VARCHAR(60))");
+              stmt.executeUpdate("insert into tbl_init values(1,\"su\", \"master\", \"access\")");
+              stmt.executeUpdate("insert into tbl_init values(2, \"api\", \"user1\", \"access2\")");
+              stmt.executeUpdate("insert into tbl_init values(3, \"weak\", \"qrcode1\", \"accesstoken1234567\")");
+              stmt.execute("CREATE TABLE tbl_hosts(id INTEGER,ipname VARCHAR(20), name VARCHAR(20), pv VARCHAR(60))");
+              stmt.executeUpdate("insert into tbl_hosts values(1, \"192.168.9.86\",\"userh2\", \"access2\")");
+              stmt.executeUpdate("backup to "+path);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InitStatic.class.getName()).log(Level.SEVERE, null, ex);
+                
+        }
+        try {
+            stmt = conn.createStatement();
+             rs = stmt.executeQuery("SELECT id FROM tbl_init WHERE name = \"master\"");
+            if(rs!=null &&rs.next()){
+            ret=true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InitStatic.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+           if(stmt!=null)try {
+               stmt.close();
+           } catch (SQLException ex) {
+               Logger.getLogger(InitStatic.class.getName()).log(Level.SEVERE, null, ex);
+           }
+           if(rs!=null)try {
+               rs.close();
+           } catch (SQLException ex) {
+               Logger.getLogger(InitStatic.class.getName()).log(Level.SEVERE, null, ex);
+           }
+        }
+        return ret;
+    }
+
+    private void savedb(Connection conn) {
+        String path=System.getProperty("user.dir")+File.separator+"rpirobotapp.db";
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("backup to "+path);
+        } catch (SQLException ex) {
+            Logger.getLogger(InitStatic.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    public static String getloginright(String _u,String _p) {
+        String ret="none";
+        ResultSet rs=null;
+        Statement stmt=null;
+        try {
+            Connection conn = CManagerMap.getCon(Info.dbcon);
+             stmt = conn.createStatement();
+             rs = stmt.executeQuery("SELECT atype FROM tbl_init WHERE name = \""+_u+"\" and pv= \""+_p+"\"");
+            if(rs!=null &&rs.next()){
+                ret=rs.getString(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InitStatic.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+           if(stmt!=null)try {
+               stmt.close();
+           } catch (SQLException ex) {
+               Logger.getLogger(InitStatic.class.getName()).log(Level.SEVERE, null, ex);
+           }
+           if(rs!=null)try {
+               rs.close();
+           } catch (SQLException ex) {
+               Logger.getLogger(InitStatic.class.getName()).log(Level.SEVERE, null, ex);
+           }
+        }
+        return ret;
+        
+    }
 
 }
